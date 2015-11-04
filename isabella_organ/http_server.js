9edd392164254a76f89c025af76af092974105c5
Isabella Organ <isabellaorgan@gmail.com>
2015-11-04 09:16:28 -0800

    add index and css base

--- isabella_organ/http_server.js
+++ isabella_organ/http_server.js
@@ -9,30 +9,45 @@ var server = http.createServer(function(req, res) {
 
 	var resData = {};
 
-	if (req.url === '/time' && req.method === 'GET') {
-		fs.readfile('./public/index.html', fucntion(err, data) {
+	if (req.url === './public/index.html' && req.method === 'GET') {
+		fs.readfile('./public/index.html', function(err, data) {
 			if (err) return console.log(err);
-		})
+		});
 		res.writeHead(200, {
 			'Content-Type': 'text/html'
 		});
-		response.write(time + ' ' + date).toString();
+		response.write('./public/index.html').toString();
 		response.end();
 	}
 
-	if (req.url === '/greet' && req.method === 'GET') {
+	if (req.url === '/time' && req.method === 'GET') {
+			if (err) return console.log(err);
+	}
 		res.writeHead(200, {
 			'Content-Type': 'text/plain'
 		});
+		response.write(time + ' ' + date).toString();
+		response.end();
+	
+
+	if (req.url === '/greet' && req.method === 'GET') {
+			if (err) return console.log(err);
 	}
+		res.writeHead(200, {
+			'Content-Type': 'text/plain'
+		});
+
+	response.write('hello' + ' ' + name).toString();
+		response.end();
+	
 
 	res.writeHead(resData.status || 404, {
 		'Content-Type': resData.contentType || 'text/plain'
 	});
 	res.Write(resData.data || 'not found');
 	res.end();
-});
+	});
 
-server.listen(3000, function() {
-	console.log('server up');
+	server.listen(3000, function() {
+		console.log('server up');
 });
