d569e8c84d4434a2c492ee49228615e27b8f01d9
anschang <anschang@hotmail.com>
2015-11-04 09:56:07 -0800

    modified server

--- ansoo_chang/server.js
+++ ansoo_chang/server.js
@@ -8,39 +8,38 @@ req is an object containing information about the HTTP request that raised the e
 
 var server = http.createServer(function(req, res) {
   // create response data object
-	var resData = {}
-	var name = req.url.split('/')[2];
-	var queryObj = url.parse(req.url, true)
+  var resData = {}
+  var name = req.url.split('/')[2];
+  var queryObj = url.parse(req.url, true)
 
-	if (req.url === '/' && req.method === 'GET') {
-		resData.status = 200;
-		resData.contentType = 'text/html';
-		resData.data = fs.readFileSync(__dirname + '/public/index.html').toString();
-	}
+  if (req.url === '/' && req.method === 'GET') {
+  	resData.status = 200;
+    resData.contentType = 'text/html';
+    resData.data = fs.readFileSync(__dirname + '/public/index.html').toString();
+  }
 
   // respond to a request to /time that will send back the current time of the server.
-	if (req.url === '/time' && req.method === 'GET') {
-	  resData.status = 200;
-	  resData.contentType = 'text/html';
-	  resData.data = new Date().toString();
+  if (req.url === '/time' && req.method === 'GET') {
+    resData.status = 200;
+    resData.contentType = 'text/html';
+    resData.data = new Date().toString();
   }
 
   // respond to a get request to /greet/name and send back a string that greets that name
   if (req.url === '/greet/' + name && req.method === 'GET') {
-    // console.log(name);
     resData.status = 200;
-	  resData.contentType = 'text/html';
-	  resData.data = 'Hello ' + name;
+    resData.contentType = 'text/html';
+    resData.data = 'Hello ' + name;
   }
 
-	res.writeHead(resData.status || 400, {
-		'Content-Type': resData.contentType || 'text/plain'
-	});
+  res.writeHead(resData.status || 400, {
+    'Content-Type': resData.contentType || 'text/plain'
+  });
 
   res.write(resData.data || 'not found');
-	res.end();
+  res.end();
 });
 
-server.listen(3000, function() {
-  console.log('server up!');
+  server.listen(3000, function() {
+    console.log('server up!');
 });
