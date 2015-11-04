2ad21565d9adef8ce82426f7308531cce48da5a0
anschang <anschang@hotmail.com>
2015-11-04 10:28:11 -0800

    added json post

--- ansoo_chang/server.js
+++ ansoo_chang/server.js
@@ -32,6 +32,13 @@ var server = http.createServer(function(req, res) {
     resData.data = 'Hello ' + name;
   }
 
+  // post request to /greet that takes the name in JSON format
+  if (req.url === '/greet' && req.method === 'POST') {
+    resData.status = 200;
+    resData.contentType = 'application/json';
+    resData.data = JSON.stringify({Hello: 'world'});
+  }
+
   res.writeHead(resData.status || 400, {
     'Content-Type': resData.contentType || 'text/plain'
   });
