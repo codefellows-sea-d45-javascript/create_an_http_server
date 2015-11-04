226836d2b1a76f162f39eb3984757ebd7078ddfa
Jack Sneed <sneed.jack@gmail.com>
2015-11-04 10:03:16 -0800

    Update index.html and server

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var http = require('http');
+var fs = require('fs');
 var date = new Date();
 var time = date.toTimeString();
 var localeDate = date.toLocaleDateString();
@@ -9,16 +10,24 @@ var server = http.createServer(function(request, response) {
 
   var urlPath = request.url.split('/');
 
-  if (request.url === '/time') {
+  if (request.url === '/') {
+    fs.readFile((__dirname + '/../lib/index.html'), function(err, data) {
+        response.writeHead(200, {
+          'Content-Type': 'text/html'
+        });
+
+        response.end(data);
+    });
+
+  } else if (urlPath[1] === 'time') {
     response.writeHead(200, {
       'Content-Type': 'text/plain'
     });
 
     response.write(time + ' ' + localeDate);
     return response.end();
-  }
 
-  if (urlPath[1] === 'greet' && request.method === 'GET') {
+  } else if (urlPath[1] === 'greet' && request.method === 'GET') {
 
     response.writeHead(200, {
       'Content-Type': 'text/plain'
@@ -26,7 +35,8 @@ var server = http.createServer(function(request, response) {
     response.write('Comment allez-vous ' + urlPath[2] + '!');
     return response.end();
 
-  } else if (request.method === 'POST') {
+  }
+      if (request.method === 'POST') {
 
       request.on('data', function(err, data) {
         var parseData = JSON.parse(data);
@@ -36,17 +46,17 @@ var server = http.createServer(function(request, response) {
         });
         response.write(JSON.stringify({name: 'Comment allez-vous ' + parseData.name}));
         return response.end();
-    });
-
-  } else {
-
-      response.writeHead(404, {
-        'Content-Type': 'text/plain'
       });
 
-      response.write('Sorry, page not found');
-      return response.end();
-    }
+      } else {
+
+        response.writeHead(404, {
+          'Content-Type': 'text/plain'
+        });
+
+        response.write('Sorry, page not found');
+        return response.end();
+      }
 });
 
 server.listen(8000, function() {
