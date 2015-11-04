7535fbbe5a0530d4b2ff267d03485c11089089bb
Emily Landi <emily.landi@gmail.com>
2015-11-04 13:11:03 -0800

    with working tests

--- emily_landi/test/test.js
+++ emily_landi/test/test.js
@@ -5,7 +5,6 @@ chai.use(chaihttp);
 
 require(__dirname + '/../server');
 
-//almost works
 describe('http server', function() {
   it('should respond to a get request to /time', function(done) {
     var date = new Date();
@@ -15,10 +14,42 @@ describe('http server', function() {
     .end(function(err, res) {
       expect(err).to.eql(null);
       expect(res.status).to.eql(200);
-      expect(res.text).to.eql(time);
+      expect(res.text).to.eql('{"time":"' + time + '"}');
+      done();
+    });
+  });
+
+  it('should respond to a post request to /greet', function(done) {
+    chai.request('localhost:3000')
+    .post('/greet')
+    .send('{"greeting":"hello bob"}')
+    .end(function(err, res) {
+      expect(err).to.eql(null);
+      expect(res.status).to.eql(200);
+      expect(res.text).to.eql('{"greeting":"hello bob"}');
+      done();
+    });
+  });
+
+  it('should respond to a get request to /greet', function(done) {
+    chai.request('localhost:3000')
+    .get('/greet')
+    .end(function(err, res) {
+      expect(err).to.eql(null);
+      expect(res.status).to.eql(200);
+      expect(res.text).to.eql('{"greeting":"hello stranger!"}');
+      done();
+    });
+  });
+
+  it('should send 404', function(done) {
+    chai.request('localhost:3000')
+    .get('/something')
+    .end(function(err, res) {
+      expect(err).to.eql(null);
+      expect(res.status).to.eql(404);
+      expect(res.text).to.eql('404 - Page cannot be found!');
       done();
     });
   });
 });
-
-
