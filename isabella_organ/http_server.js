fc128c9399441498008f269dbbaffeb8fe78e20d
Isabella Organ <isabellaorgan@gmail.com>
2015-11-04 06:56:35 -0800

    work on read index, get date

--- isabella_organ/http_server.js
+++ isabella_organ/http_server.js
@@ -1,17 +1,23 @@
 var http = require('http');
 var fs = require('fs');
-var time = ;
-var date = ;
-var ReadStream = require('stream').Readable;
+var url = require('url');
+// var time = ;
+// var date = ;
+
 
 var server = http.createServer(function(req, res) {
 
 	var resData = {};
 
 	if (req.url === '/time' && req.method === 'GET') {
+		fs.readfile('./public/index.html', fucntion(err, data) {
+			if (err) return console.log(err);
+		})
 		res.writeHead(200, {
-			'Content-Type': 'text/plain'
+			'Content-Type': 'text/html'
 		});
+		response.write(time + ' ' + date).toString();
+		response.end();
 	}
 
 	if (req.url === '/greet' && req.method === 'GET') {
