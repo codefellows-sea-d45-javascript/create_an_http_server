96408b317f0cd663f956c5ea883588f3fabd26d3
Matthew Ringel <mattew.r.ringel@gmail.com>
2015-11-04 11:16:28 -0800

    passing three tests

--- matthew_ringel/test/server_test.js
+++ matthew_ringel/test/server_test.js
@@ -7,7 +7,36 @@ require(__dirname + '/../index');
 
 describe('our server', function() {
 
-  it('should respond to a request to /time', function() {
-    
-  } )
-})
+  it('should respond to a GET request to /time', function(done) {
+    chai.request('localhost:3000')
+      .get('/time')
+      .end(function(error, response) {
+          expect(error).to.eql(null);
+          expect(response).to.have.status(200);
+          done();
+      });
+  });
+
+  it('should respond to a GET request to /greet/[name]', function(done) {
+    chai.request('localhost:3000')
+      .get('/greet/name')
+      .end(function(error, response) {
+        expect(error).to.eql(null);
+        expect(response).to.have.status(200);
+        done();
+      });
+  });
+
+  it('should respond to a POST request to /greet with JSON data', function(done) {
+    chai.request('localhost:3000')
+      .post('/greet')
+      .send({"name": "bob"})
+      .end(function(error, response) {
+        expect(error).to.eql(null);
+        expect(response).to.have.status(200);
+        done();
+      });
+  });
+
+
+});
