21bbc7c9198fad6ca6709b1a27bf31d6f8c58a9d
Rick Patci <rpatci@gmail.com>
2015-11-04 13:52:15 -0800

    fix test and css

--- rick_patci/server.js
+++ rick_patci/server.js
@@ -11,15 +11,26 @@ var routesAvailable = {
 
 var server = http.createServer(function(req, res) {
   var resData = {};
+
   if(req.url === routesAvailable.root && req.method === 'GET') {
     resData.status = 200;
     resData.contentType = 'text/html';
     resData.data = fs.readFileSync(__dirname + '/lib/index.html').toString();
   }
+  if(req.url === '/style.css') {
+    res.writeHead(200, {
+      'Content-Type': 'text/css'
+    });
+    res.write(fs.readFileSync(__dirname + '/lib/style.css'));
+      return res.end();
+  }
   if(req.url === routesAvailable.currentTime && req.method === 'GET') {
-      resData.status = 200;
-      resData.contentType = 'text/plain';
-      resData.data = new Date().getTime().toString();
+    resData.status = 200;
+    resData.contentType = 'text/plain';
+    resData.data = new Date().getTime().toString();
+  }
+  if(req.url === routesAvailable.greet && req.method === 'POST') {
+
   }
   res.writeHead(resData.status || 400, {
     'Content-Type' : resData.contentType || 'text/plain'
