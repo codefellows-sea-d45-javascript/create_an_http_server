65f09b2ba0ee8354138818fdc736faf12b5abc93
adam-james <adam.j.sulewski@gmail.com>
2015-11-04 08:53:26 -0800

    Remove instructions from notes

--- adam_sulewski/server.js
+++ adam_sulewski/server.js
@@ -1,14 +1,5 @@
 'use strict';
 
-//  ROUTES:
-//    /TIME SENDS CURRENT TIME
-//    /GREET/NAME GREETS NAME PARAM IN URL
-//    POST TO GREET/ HANDLES JSON AND GREETS JSON
-//  TEST USING CHAI-HTTP
-//  BONUS POINTS:
-//    AUTOPOPULATE ROUTES IN INDEX.HTML
-//    STYLE INDEX.HTML
-
 var http = require('http');
 var route = require(__dirname + '/lib/route');
 var PORT = 3000;
