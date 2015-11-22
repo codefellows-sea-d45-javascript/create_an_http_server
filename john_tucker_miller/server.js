98c7b0fea3ff060b295298fdeccdbfcbd992849d
jhnmllr <tucker.miller@gmail.com>
2015-11-21 19:50:23 -0800

    added testing and gulpfile

--- john_tucker_miller/server.js
+++ john_tucker_miller/server.js
@@ -1,19 +1,30 @@
 var http = require('http');
 var fs = require('fs');
 var ReadStream = require('stream');
+var url = require('url');
+var express = require('express');
+var app = module.exports = express();
+var bodyParser = require('body-parser').json;
 
-var server = http.createServer(function(req, res) {
-  var resdata = {};
-  if (req.url === '/time' && req.method === 'GET') {
-    // respond with the server's time
-  }
+app.use(bodyParser());
 
-  if (req.url === '/greet' && req.method === 'GET') {
-    // respond with a greeting
-  }
-
+app.get('/time', function(req, res) {
+  var date = new Date();
+  res.send(date.toDateString());
+  res.end();
 });
 
-server.listen(3000, function() {
-  console.log('server up');
+app.get('/greet/:name', function (req, res) {
+  res.send('yo whats up ' + req.params.name);
+  res.end();
+});
+
+app.post('/greet', function (req, res) {
+
+  res.send('yo whats up ' + req.body.name);
+  res.end();
+});
+
+app.listen(3000, function() {
+  console.log('server is ready');
 });
