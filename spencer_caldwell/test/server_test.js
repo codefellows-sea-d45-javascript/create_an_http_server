450e1892bbbf28e99a0705a392dc83529ee67b66
Spencer Caldwell <spencer.caldwell@gmail.com>
2015-12-30 21:46:48 -0800

    added 2 more tests

--- spencer_caldwell/test/server_test.js
+++ spencer_caldwell/test/server_test.js
@@ -21,4 +21,26 @@ describe('our server', function() {
       }.bind(this));
   });
 
+  it('should respond to /time by sending back the current time', function(done) {
+    chai.request('localhost:3000')
+      .get('/time')
+      .end(function(err, res) {
+        expect(err).to.eql(null);
+        expect(res).to.have.status(200);
+        expect(res.text).to.be.above(1451540386398);
+        done();
+      });
+  });
+
+  it('should respond to /greet/* with the name you send it', function(done) {
+    chai.request('localhost:3000')
+      .get('/greet/test')
+      .end(function(err, res) {
+        expect(err).to.eql(null);
+        expect(res).to.have.status(200);
+        expect(res.text).to.eql('Hello test!');
+        done();
+      });
+  });
+
 });
